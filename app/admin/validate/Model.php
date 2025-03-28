80feaa2b9425b74fdd2f28dd75f0d86adacb3afb
xcl1224 <gjcnsfla1224@naver.com>
2025-03-28 18:49:34 +0900

    2025.3.28

--- app/admin/validate/Model.php
+++ app/admin/validate/Model.php
@@ -11,22 +11,24 @@ class Model extends BaseValidate
         'model_type' => 'require',
         'name' => 'require',
         'menu' => 'require',
-        'status' => 'require'
+        'status' => 'require',
+        'ranking'=>'require'
     ];
 
     protected $message = [
-        'id' => '',
-        'pid' => '',
-        'model_type' => '',
-        'name' => '',
-        'menu' => '',
-        'status' => ''
+        'id' => '请填写ID_code001',
+        'pid' => '请填写父级ID_code002',
+        'model_type' => '请选择菜单或模型_code003',
+        'name' => '请填写模型名称_code004',
+        'menu' => '请填写菜单名称_code005',
+        'status' => '请选择状态_code006',
+        'ranking'=>'请排优先级_code007'
     ];
 
     protected $scene = [
         'index' => ['page'],
-        'add' => ['pid','model_type','status'],
-        'update' => ['id','pid','model_type','status'],
+        'add' => ['pid','model_type','status','ranking'],
+        'update' => ['id','pid','model_type','status','ranking'],
         'updateStatus' => ['id','status'],
         'delete' => ['id']
     ];
