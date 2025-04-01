270a795dae2ec5c8b18bd014aaae6d351c911e9b
xcl1224 <gjcnsfla1224@naver.com>
2025-04-01 23:36:27 +0900

    2025.4.1 home

--- app/admin/model/GoodsSpec.php
+++ app/admin/model/GoodsSpec.php
@@ -4,5 +4,24 @@ namespace app\admin\model;
 
 class GoodsSpec extends BaseM
 {
+     public function childs(){
+         return $this->hasMany('GoodsSpec','spec_id','id');
+     }
 
+     public function deleteChild($ids){
+         $this->WhereIn('id',$ids)->delete();
+     }
+
+     public function getAll($page,$limit=10){
+          $list = $this->select();
+          $menus = $this->page($page,$limit)->where('spec_id',0)->select();
+          $total = $this->count();
+          return ['list'=>$list,'menus'=>$menus,'total'=>$total];
+     }
+
+    protected static function onBeforeDelete($spec){
+        //자식아이디만 갖고오는 함수 getAllChildIds 중요점 static이므로 this 대신 변수 $category로 다른 함수를 실행시켜야합니다
+        $childs_ids = $spec->getAllChildIds($spec->id,'childs');
+        $spec->deleteChild($childs_ids);
+    }
 }
--- app/admin/service/GoodsSpec.php
+++ app/admin/service/GoodsSpec.php
@@ -4,23 +4,39 @@ namespace app\admin\service;
 
 class GoodsSpec extends BaseService
 {
-    public function getAll(){
-
+    public function getAll($page,$limit = 10){
+        return $this->M->getAll($page,$limit);
     }
 
-    public function add(){
-
+    public function add($data){
+        if($data['spec_id'] != 0){
+            $spec_menu = $this->M->where('spec_id',$data['spec_id'])->column('spec_menu');
+            if($spec_menu=== null){
+                ApiException("选择的父级菜单不存在！");
+            }
+            if($spec_menu == 0){
+                ApiException("想添加的必须菜单才可以，配置下添加不了");
+            }
+        }
+        if($data['spec_id'] == 0 && $data['spec_menu'] == 0){
+            ApiException("配置必须在菜单下面添加才可以！");
+        }
+        $data['ram'] = json_encode($data['ram']);
+        $data['storage'] = json_encode($data['storage']);
+        return $this->M->MPsave($data);
     }
 
-    public function update(){
-
+    public function update($data){
+        $data['ram'] = json_encode($data['ram']);
+        $data['storage'] = json_encode($data['storage']);
+        return request()->Model->save($data);
     }
 
-    public function updateStatus(){
-
+    public function updateStatus($status){
+        return $this->M->updateStatus(['status'=>$status]);
     }
 
     public function delete(){
-
+        return $this->M->MPdelete();
     }
 }
