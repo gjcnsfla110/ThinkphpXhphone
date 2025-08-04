d941f0eab1453007371eef3fa53f495d7e5a6b9c
xcl1224 <gjcnsfla1224@naver.com>
2025-08-04 20:00:04 +0900

    2025-8-5

--- app/admin/model/ComponentItem.php
+++ app/admin/model/ComponentItem.php
@@ -3,6 +3,7 @@
 namespace app\admin\model;
 use app\admin\model\goodsCategory;
 use app\admin\model\GoodsSubmenu;
+use app\admin\model\Goods;
 class ComponentItem extends BaseM
 {
      public function getList($id){
@@ -12,13 +13,15 @@ class ComponentItem extends BaseM
             ];
      }
     public function getGoodsList($page, $limit){
-          $list = $this->order(['ranking'=>'desc','id'=>'desc'])->page($page, $limit)->where(['status',1])->select();
-          $total = $this->where('status',1)->count();
-          $mainMenu = goodsCategory::select()->toArray();
+          $list = Goods::page($page, $limit)->where(['status'=>1])->order(['order'=>'desc','id'=>'desc'])->select();
+          $total = Goods::where('status',1)->count();
+          $mainMenu = goodsCategory::select()->where('category_id',0)->toArray();
+          $menuList = goodsCategory::select()->toArray();
           $subMenu = GoodsSubmenu::select()->toArray();
           return [
               'list'=>$list,
               'total'=>$total,
+              'menuList' => $menuList,
               'mainMenu'=>$mainMenu,
               'subMenu'=>$subMenu
           ];
